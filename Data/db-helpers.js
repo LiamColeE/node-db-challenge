be4e007fe9276d80706a164d11d30e72294d3182
LiamColeE <liamedlinger@gmail.com>
2019-11-23 13:38:31 -0600

    getting real close

--- Data/db-helpers.js
+++ Data/db-helpers.js
@@ -5,7 +5,13 @@ const db = knex(config.development);
 module.exports = {
     findProjects,
     findProjectsByID,
-    createProject
+    createProject,
+    createResource,
+    findResources,
+    findResourcesByID,
+    findTasks,
+    createTask,
+    findTasksByID
 }
 
 function findProjects(){
@@ -17,5 +23,34 @@ function findProjectsByID(id){
 }
 
 function createProject(project){
-    return db('projects').insert(project)
+    return db('projects').insert(project);
+}
+
+function createResource(resource){
+    return db('resources').insert(resource);
+}
+
+function findResources(){
+    return db('resources');
+}
+
+function findResourcesByID(id){
+    return db('resources').where({resource_id:id});
+}
+
+function findTasks(){
+    return db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select('t.name', 't.description', 'p.project_name', 'p.project_description')
+}
+
+function findTasksByID(id){
+    return db('tasks as t')
+    .where({task_id:id})
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select('t.name', 't.description','t.completed', 'p.project_name', 'p.project_description')
+}
+
+function createTask(task){
+    return db('tasks').insert(task);
 }
--- server.js
+++ server.js
@@ -17,17 +17,86 @@ server.post('/api/projects', (req,res) => {
     let project = {
         project_name: req.headers.project_name,
         project_description: req.headers.project_description,
-        completed: false
+        completed: req.headers.completed
     }
-
     db.createProject(project)
     .then((result) => {
-        res.status("200")
-        res.send(result)
+        db.findProjectsByID(result[0])
+        .then((newProject) => {
+            res.status("200")
+            res.send(newProject)
+        })
+
     })
     .catch((err) => {
         res.status("500");
         res.send("internal server err")
+    })
+})
+
+server.get('/api/projects', (req,res) => {
+    db.findProjects()
+    .then((result) => {
+        res.send(result)
+        res.status(200);
+    })
+})
+
+server.post('/api/resources', (req,res) => {
+    let resource = {
+        name: req.headers.name,
+        description: req.headers.description
+    }
+
+    db.createResource(resource)
+    .then((result) => {
+        db.findResourcesByID(result[0])
+            .then((result) => {
+            res.send(result)
+            res.status(200)
+        })
+    })
+    .catch((err) => {
+        res.send("internal server error")
+        res.status(500);
+    })
+})
+
+server.get('/api/resources', (req,res) => {
+    db.findResources()
+    .then((result) => {
+        res.send(result)
+        res.status(200)
+    })
+    .catch((err) => {
+        res.send("internal server error")
+        res.status(500);
+    })
+})
+
+server.get('/api/tasks', (req,res) => {
+    db.findTasks()
+    .then((result) => {
+        res.send(result)
+        res.status(200)
+    })
+})
+
+server.post('/api/tasks', (req,res) => {
+    let task = {
+        name: req.headers.name,
+        description: req.headers.description,
+        project_id: req.headers.project_id,
+        completed: req.headers.completed
+    }
+
+    db.createTask(task)
+    .then((result) => {
+        db.findTasksByID(result[0])
+        .then((newTask) => {
+            res.send(newTask)
+            res.status(200)
+        })
     })
 })
 
